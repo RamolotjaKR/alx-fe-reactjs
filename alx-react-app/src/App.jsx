71a61dc5ea65fc4802fd5166fdba07964dbcf770
Kabelo Ramolotja <kr14rams@gmail.com>
2025-10-30 18:52:22 +1300

    Added components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div className="app">
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
