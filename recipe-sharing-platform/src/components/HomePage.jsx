550d1a069783cbab92be0b724d22a4a1063f68f3
Kabelo Ramolotja <kr14rams@gmail.com>
2025-12-03 11:00:17 +1300

    detail page

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import recipeData from '../data.json';
 
 const HomePage = () => {
