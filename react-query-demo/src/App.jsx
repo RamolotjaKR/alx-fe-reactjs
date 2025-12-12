36ebc871059ce7629601bfc693e94f611e505c08
Kabelo Ramolotja <kr14rams@gmail.com>
2025-12-12 22:35:26 +1300

    query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+const queryClient = new QueryClient()
+
+function PostsComponent() {
+  return (
+    <div>
+      <h2>Posts Component</h2>
+    </div>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +39,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      <PostsComponent />
+    </QueryClientProvider>
   )
 }
 
